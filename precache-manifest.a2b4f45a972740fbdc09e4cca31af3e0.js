ff1a9093700d22db29d9cd54882c9cc416045e36
brazill91 <brazill91@gmail.com>
2020-08-22 23:25:47 -0400

    Updates

---
+++ precache-manifest.a2b4f45a972740fbdc09e4cca31af3e0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1deb950843600c8b34b554198f2a1c4b",
+    "url": "/bm2/index.html"
+  },
+  {
+    "revision": "eff6459b51f339d9f5e8",
+    "url": "/bm2/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2d9719649abb043d09dc",
+    "url": "/bm2/static/css/main.47b80a1a.chunk.css"
+  },
+  {
+    "revision": "eff6459b51f339d9f5e8",
+    "url": "/bm2/static/js/2.5d747e2b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/bm2/static/js/2.5d747e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9719649abb043d09dc",
+    "url": "/bm2/static/js/main.21d0973a.chunk.js"
+  },
+  {
+    "revision": "255f8ee84d7be4095185",
+    "url": "/bm2/static/js/runtime-main.e15f9c1b.js"
+  },
+  {
+    "revision": "4cf3b44464563872dc0f3da31b0f37c3",
+    "url": "/bm2/static/media/analysis.4cf3b444.jpg"
+  },
+  {
+    "revision": "1adaf4e8a03ce2bd2b74ac12d7929db5",
+    "url": "/bm2/static/media/atomistLanding.1adaf4e8.jpg"
+  },
+  {
+    "revision": "bb068191eb3e5114efa3900bd57446b3",
+    "url": "/bm2/static/media/atomistLandingResponsive.bb068191.jpg"
+  },
+  {
+    "revision": "29797f62d2a837f5672ed650a56b0cf0",
+    "url": "/bm2/static/media/full.29797f62.jpg"
+  },
+  {
+    "revision": "a2f23793b8b504517cc92ec90365da87",
+    "url": "/bm2/static/media/full14.a2f23793.jpg"
+  },
+  {
+    "revision": "b0d9ece4b4558df380e22012dc395b05",
+    "url": "/bm2/static/media/full20.b0d9ece4.jpg"
+  },
+  {
+    "revision": "51577f0767adf8184e9356e38a64b61d",
+    "url": "/bm2/static/media/full8.51577f07.jpg"
+  },
+  {
+    "revision": "a6bb38e8a1d2a4b06459899773d63fe8",
+    "url": "/bm2/static/media/fullEmpty.a6bb38e8.jpg"
+  },
+  {
+    "revision": "3ddc59083109fe6855fb818bf536f262",
+    "url": "/bm2/static/media/fullEmpty20.3ddc5908.jpg"
+  },
+  {
+    "revision": "bd6c8edbb25d78ace30861a363cfe347",
+    "url": "/bm2/static/media/geckoLanding.bd6c8edb.jpg"
+  },
+  {
+    "revision": "990a4c21c3adcbf76cd4ba459e8d359a",
+    "url": "/bm2/static/media/geckoMain.990a4c21.jpg"
+  },
+  {
+    "revision": "10b747d47eb9751ec11569cf116af3a9",
+    "url": "/bm2/static/media/geckoResponsive.10b747d4.jpg"
+  },
+  {
+    "revision": "e46682633f24386116a0c2ef9dd53891",
+    "url": "/bm2/static/media/gmail.e4668263.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bm2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bcc877fcc29396c2532d0e28f0b4f6a3",
+    "url": "/bm2/static/media/paintStyle.bcc877fc.jpg"
+  },
+  {
+    "revision": "d7a02dbb9d4b1bc87ffd4349442266c4",
+    "url": "/bm2/static/media/pricing.d7a02dbb.jpg"
+  },
+  {
+    "revision": "ac7f1e01068c553f3cfef21e955962ae",
+    "url": "/bm2/static/media/pricingResponsive.ac7f1e01.jpg"
+  },
+  {
+    "revision": "6e4b91e5fbf7b0a95fa98610033dc999",
+    "url": "/bm2/static/media/signIn.6e4b91e5.jpg"
+  },
+  {
+    "revision": "701a4bff6192a302cc66c314334c229f",
+    "url": "/bm2/static/media/webPost.701a4bff.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bm2/precache-manifest.a2b4f45a972740fbdc09e4cca31af3e0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bm2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
