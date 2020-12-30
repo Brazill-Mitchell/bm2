5650503b5aeedeb5fbdec2fac522c4c8a0425a85
brazill91 <brazill91@gmail.com>
2020-12-30 01:19:42 -0500

    edits gitignore to push dev resources/

---
+++ dev resources/desktopIntro/desktopIntro.jsx
@@ -0,0 +1,59 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./desktopIntro.css";
+
+class DesktopIntro extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="f74c10d6-2116-4706-8dec-8d81a78b2687" className="desktopIntro">        <div data-layer="1f8e5d81-a3f0-4a9f-9342-fc29f5e23909" className="scrollProjectArea">            <div data-layer="45bbd971-1f5d-40bf-ba11-0b14aa700cf5" className="scrollingRectangle"></div>
+            <div data-layer="c0f2eab9-e0b5-4516-a5c3-7de1db647272" className="maskGroup1"></div>
+</div>
+        <div data-layer="f150124c-641a-4eff-8d37-7c0ac90a9bf1" className="nav">            <div data-layer="dbec65f2-e8f8-4db5-bc85-3e48f48ae320" className="navRec"></div>
+            <div data-layer="84992b7c-9341-48ea-8f7d-b604e99d5f86" className="home">Home</div>
+            <div data-layer="b6437915-bcbf-4e1a-9ad4-0aa4cc3e7909" className="about">About</div>
+            <div data-layer="aad565f0-c75d-4ae1-991d-12728f54d794" className="projects">Projects</div>
+            <div data-layer="450ceff2-8f37-4f3a-bf94-740392dff60b" className="contact">Contact</div>
+</div>
+        <div data-layer="433c133d-1376-4bf1-8c71-ab18cd45ca60" className="intro11733b9a">            <div data-layer="7c91c95a-867d-497f-8bca-82eb8957fc2b" className="intro">INTRO</div>
+            <div data-layer="d7e199ee-c10c-4318-8d56-20f47529c719" className="someTextHereGivingABriefIntroductionToMeAndMeProjects">Some text here giving a brief introduction to me and me projects</div>
+</div>
+        <div data-layer="0e305aea-e84f-429e-af70-28409363dbcb" className="scrollProjectSummary">            <div data-layer="f124fb60-33ba-483a-b5c1-6846285a8146" className="links">                <div data-layer="963a1b45-9af8-4a3a-85a0-561d139ad360" className="projectName0db1b482">Project name</div>
+                <div data-layer="953cee79-1d51-4c96-98c0-76a245743e92" className="link204196014">                    <div data-layer="2963779b-7e72-45cb-9b99-03d61fd76ee9" className="link2">link2</div>
+                    <div data-layer="fc50da61-a47a-4381-92ab-945c2156a203" className="githubMark"></div>
+</div>
+                <div data-layer="7853d0c2-bc54-4f10-a485-b4857847ba48" className="link1">                    <div data-layer="28268ce4-03c2-47cc-bc2c-1e1bdcf811b6" className="projectName">Project Name</div>
+                    <svg data-layer="81c8f23d-5e81-4bb1-b320-88ebc07ae79e" preserveAspectRatio="none" viewBox="-0.3470458984375 -0.35992431640625 28.694091796875 27.7198486328125" className="line1"><path d="M 0 27 L 28 0"  /></svg>
+                    <svg data-layer="66b5647b-571a-4596-9ee5-8fc5689fcd49" preserveAspectRatio="none" viewBox="0 -0.5 23 1" className="line2"><path d="M 0 0 L 23 0"  /></svg>
+                    <svg data-layer="0a42bd4a-ff9b-4b12-a7a2-2f3a754d05de" preserveAspectRatio="none" viewBox="-0.5 0 1 21" className="line3"><path d="M 0 21 L 0 0"  /></svg>
+</div>
+</div>
+            <div data-layer="fc311667-5fa8-4e8a-bf87-0e5523d60433" className="aBriefSummaryOfTheProject">A brief summary of the project</div>
+</div>
+</div>
+
+    );
+  }
+}
+
+DesktopIntro.propTypes = {
+
+}
+
+DesktopIntro.defaultProps = {
+
+}
+
+
+export default DesktopIntro;
+          
